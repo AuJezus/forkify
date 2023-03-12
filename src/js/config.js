64a1f9284ad43877fef7734faae3cee95d43c9b1
aujezus <augispay@gmail.com>
2023-03-12 17:25:34 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `9b5081d0-a3d3-4c59-92dc-204be1ff4650`;
+export const MODAL_CLOSE_SEC = 2.5;
